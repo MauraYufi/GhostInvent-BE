79da7a14df64e4ff4d784f15d67a141800151ee4
Hervi Nur Rahmandien <71449881+hervibest@users.noreply.github.com>
2023-06-04 16:05:21 +0700

    Add integration API for update balance data

--- controllers/balanceController.js
+++ controllers/balanceController.js
@@ -3,11 +3,29 @@ const Balance = require("../models/balanceModel");
 
 // Get all Products
 const getBalances = asyncHandler(async (req, res) => {
-    const balances = await Balance.find({ user: req.user.id }).sort("-createdAt");
-    res.status(200).json(balances);
-  });
+  const balances = await Balance.find({ user: req.user.id }).sort("-createdAt");
+  res.status(200).json(balances);
+});
+
+
+const updateBalances = asyncHandler(async (req, res) => {
+  const { balance } = req.body;
+
+  // Update the balance in MongoDB
+  const updatedBalance = await Balance.findOneAndUpdate(
+    {  user: "647b493048d50346267c6ac3" },
+    { balance },
+    { new: true }
+  );
+
+  if (!updatedBalance) {
+    return res.status(404).json({ error: "Balance not found" });
+  }
+
+  res.status(200).json(updatedBalance);
+});
 
 module.exports = {
-    getBalances
+  getBalances,
+  updateBalances,
 };
-  
--- routes/balanceRoute.js
+++ routes/balanceRoute.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/authMiddleware");
 const {
-  getBalances,
+  getBalances, updateBalances
 } = require("../controllers/balanceController");
 
 router.get("/", protect, getBalances);
+router.post("/post", updateBalances);
 
 module.exports = router;
